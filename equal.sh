08fe5b7cd75d2448cac0182ff6db6436b3238299
Roopa2487 <154946002+Roopa2487@users.noreply.github.com>
2024-01-11 18:42:47 +0530

    Update equal.sh

--- equal.sh
+++ equal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Enter the 1st number :"
-read num1
-echo "Enter the 2nd number :"
-read num2
+#echo "Enter the 1st number :"
+#read num1
+#echo "Enter the 2nd number :"
+#read num2
 if [ $num1 -eq $num2 ]
 then
 	echo "$num1 is equal to $num2"
